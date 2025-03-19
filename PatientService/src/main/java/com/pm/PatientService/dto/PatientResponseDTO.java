ba3c97e372b6589097d3fe7c261f959d11f016f1
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-03-19 09:18:53 -0400

    PatientResponseDTO created

---
+++ PatientService/src/main/java/com/pm/PatientService/dto/PatientResponseDTO.java
@@ -0,0 +1,9 @@
+package com.pm.PatientService.dto;
+
+public class PatientResponseDTO {
+    private String Id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+}
